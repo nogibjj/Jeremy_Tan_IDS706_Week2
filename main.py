648ec75abe182f43042cc58621d11287e068e4e2
jeremymtan <jemtan@ucsc.edu>
2023-09-09 16:57:16 -0400

    revise for new requirements

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pandas import DataFrame
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
 
 
 def describe_iris(summary):
@@ -23,11 +25,58 @@ def describe_iris(summary):
         )
     )
 
-    return iris.describe(include="all").loc[summary]
+    return iris.median(), iris.describe(include="all").loc[summary]
 
 
 def general_describe(csv):
     """general describe function in csv"""
     general_df = pd.read_csv(csv)
-
     return general_df.describe()
+
+
+def generate_viz_iris():
+    """generates example visualziation for iris dataset"""
+    iris_obj = load_iris()
+
+    iris = DataFrame(
+        iris_obj["data"],
+        columns=iris_obj["feature_names"],
+        index=pd.Index([i for i in range(iris_obj["data"].shape[0])]),
+    )
+
+    iris.hist(edgecolor="black", linewidth=1)
+    plt.show()
+
+
+def generate_vis_general_congress(csv):
+    """generate example visualization of the congress dataset"""
+    pd.set_option("display.max_columns", None)
+    general_df = pd.read_csv(csv)
+    # print(general_df.head())
+    print(general_df.describe())
+    plt.figure(figsize=(10, 6))
+    plt.hist(general_df["age"], bins=20, edgecolor="black")
+    plt.title("Age Distribution of Congress Members")
+    plt.xlabel("Age")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def generate_summary_iris():
+    """generates report for iris dataset"""
+    iris_obj = load_iris()
+
+    iris = DataFrame(
+        iris_obj["data"],
+        columns=iris_obj["feature_names"],
+        index=pd.Index([i for i in range(iris_obj["data"].shape[0])]),
+    )
+    profile = ProfileReport(iris, title="Profiling Report")
+    profile.to_file("iris.html")
+
+
+def generate_summary(csv):
+    """generates report of any dataset"""
+    general_df = pd.read_csv(csv)
+    profile = ProfileReport(general_df, title="Profiling Report")
+    profile.to_file("profile.html")
--- test_main.py
+++ test_main.py
@@ -3,24 +3,51 @@ Test goes here
 
 """
 
-from main import describe_iris, general_describe
+from main import (
+    describe_iris,
+    general_describe,
+    generate_viz_iris,
+    generate_vis_general_congress,
+    generate_summary,
+    generate_summary_iris,
+)
+import pandas as pd
 
-example_csv = "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2021-financial-year-provisional/Download-data/annual-enterprise-survey-2021-financial-year-provisional-csv.csv"
+example_csv = "https://raw.githubusercontent.com/fivethirtyeight/data/master/congress-age/congress-terms.csv"
 
 
 def test_describe_iris_and_general_describe():
-    """Function calling describe_iris and tests different parts of the dataset"""
+    """Function calling describe_iris and general_describe which tests different parts of the dataset"""
 
-    # print(describe_iris("std"))
-
-    assert describe_iris("mean").loc["sepal length (cm)"] == 5.843333333333334
-    assert describe_iris("max").loc["species"] == 2.0
-    assert describe_iris("std").loc["petal width (cm)"] == 0.7622376689603465
+    # for the first assert statement I am calling the mean but the function is returning the medians for all the columns
+    # so returns tuple of (median of all columns, .decribe() function)
+    assert describe_iris("mean")[0].loc["sepal length (cm)"] == 5.8
+    assert describe_iris("mean")[1].loc["petal width (cm)"] == 1.1993333333333336
+    assert describe_iris("std")[1].loc["petal width (cm)"] == 0.7622376689603465
 
     # only works for the example_csv link
-    assert general_describe(example_csv).loc["mean", "Year"] == 2017.0
-    assert general_describe(example_csv).loc["count", "Year"] == 41715.0
+    # since the congress dataset has columns that don't contain numeric data, I can't just return .median()
+    # so I need to read it here to check
+    congress = pd.read_csv(example_csv)
+    median_age = congress["age"].median()
+    assert median_age == 53.0
+    assert general_describe(example_csv).loc["mean", "age"] == 53.31373222430909
+    assert general_describe(example_csv).loc["std", "age"] == 10.678469157541091
+
+
+def test_viz_iris_and_general():
+    """Function calling generate_viz_iris() and generate_vis_general_congress"""
+    generate_viz_iris()
+    generate_vis_general_congress(example_csv)
+
+
+def test_generate_summary_report():
+    """Function calling generate_summary() and generate_summary_iris()"""
+    generate_summary(example_csv)
+    generate_summary_iris()
 
 
 if __name__ == "__main__":
     test_describe_iris_and_general_describe()
+    # test_viz_iris_and_general()
+    # test_generate_summary_report()
